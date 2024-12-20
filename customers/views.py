5f1e173b3819f0b23f97dbfa579920d81e6d83d8
Adedayo Adegbola <adegboladayor@gmail.com>
2024-12-20 01:10:20 +0100

    feat: created user register endpoint

--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,11 @@
 from customers.models import Customer
 from django.http import JsonResponse, Http404
-from customers.serializers import CustomerSerializer
+from customers.serializers import CustomerSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -12,7 +13,7 @@ def customers(request):
   if request.method == 'GET':
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
-    return Response({'Customers': serializer.data})
+    return Response({'customers': serializer.data})
   elif request.method == "POST":
     serializer = CustomerSerializer(data=request.data)
     if serializer.is_valid():
@@ -31,7 +32,7 @@ def customer(request, id):
   
   if request.method == 'GET':
     serializer = CustomerSerializer(data)
-    return Response({'Customer': serializer.data})
+    return Response({'customer': serializer.data})
   elif request.method == 'DELETE':
     data.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
@@ -39,5 +40,18 @@ def customer(request, id):
     serializer = CustomerSerializer(data, data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return Response({'Customer': serializer.data})
+      return Response({'customer': serializer.data})
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+@api_view(['POST'])
+def register(request):
+  serializer = UserSerializer(data=request.data)
+  if serializer.is_valid():
+    user = serializer.save()
+    refresh = RefreshToken.for_user(user)
+    tokens = {
+      'refresh': str(refresh),
+      'access': str(refresh.access_token)
+    }
+    return Response(tokens, status=status.HTTP_201_CREATED)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
